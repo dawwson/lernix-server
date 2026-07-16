31cec9ac721087c65d970798a4b1a855723a3e7f
dawwson <shines1427@gmail.com>
2026-07-16 17:45:04 +0900

    refactor(cart): course outbound adapter에서 user 의존성 제거

--- src/main/java/com/lxp/aplus/cart/application/port/out/course/CartCourseQueryPort.java
+++ src/main/java/com/lxp/aplus/cart/application/port/out/course/CartCourseQueryPort.java
@@ -10,12 +10,12 @@ public interface CourseQueryPort {
 
     // TODO: model 네이밍 구체적으로 바꿀 필요성
     /*
-     * course별 구매 정보(가격, 구매 가능 여부) 조회
+     * 판매 정보 조회
      */
     Map<Long, CourseSalesStatus> getCourseSalesStatusByIds(List<Long> courseIds);
 
     /*
-     * course의 구매 관련 상세 정보 조회
+     * 구매 상세 정보 조회
      */
     Map<Long, CourseSnapshot> getCourseSnapshotByIds(List<Long> courseIds);
 }
