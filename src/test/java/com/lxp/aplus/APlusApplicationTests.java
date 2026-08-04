66d891052a6b05a74173d9a36128e206449b1b96
dawwson <shines1427@gmail.com>
2026-08-04 19:38:10 +0900

    test: application-test.yml을 사용하도록 고정

--- src/test/java/com/lxp/aplus/APlusApplicationTests.java
+++ src/test/java/com/lxp/aplus/APlusApplicationTests.java
@@ -2,8 +2,10 @@ package com.lxp.aplus;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class APlusApplicationTests {
 
     @Test
