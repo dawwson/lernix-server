295ee51637b6baa19f96864566cff3730e43194f
dawwson <shines1427@gmail.com>
2026-08-05 13:22:56 +0900

    feat(payment): 재결제가 허용되지 않는 상태를 위한 오류 추가

--- src/main/java/com/lxp/aplus/common/error/code/PaymentErrorCode.java
+++ src/main/java/com/lxp/aplus/common/error/code/PaymentErrorCode.java
@@ -15,7 +15,9 @@ public enum PaymentErrorCode implements ErrorCode {
     PAYMENT_AMOUNT_MISMATCH(HttpStatus.CONFLICT, "EPM004", "승인 요청된 금액이 결제 예상 금액과 일치하지 않습니다."),
     PAYMENT_ALREADY_APPROVED(HttpStatus.CONFLICT, "EPM005", "이미 승인 처리된 결제 건입니다."),
     PAYMENT_NOT_FOUND(HttpStatus.NOT_FOUND, "EPM006", "결제 정보를 찾을 수 없습니다."),
-    PAYMENT_ACCESS_DENIED(HttpStatus.FORBIDDEN, "EPM007", "해당 결제에 대한 접근 권한이 없습니다.");
+    PAYMENT_ACCESS_DENIED(HttpStatus.FORBIDDEN, "EPM007", "해당 결제에 대한 접근 권한이 없습니다."),
+    PAYMENT_RETRY_NOT_ALLOWED(HttpStatus.CONFLICT, "EPM008", "이미 처리된 주문은 다시 결제할 수 없습니다."),
+    PAYMENT_ORDER_MISMATCH(HttpStatus.CONFLICT, "EPM009", "결제 정보와 주문 정보가 일치하지 않습니다.");
 
     private final HttpStatus status;
     private final String code;
